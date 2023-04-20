558944e84bd7f56a51086e6ea3fe3e80ee35c1de
Liu, Zhao-hui <lzhh@duck.com>
2023-04-20 14:00:57 +0800

    Fix that `skip' and `subtype' are not cleared

--- mvsubs.bat
+++ mvsubs.bat
@@ -5,6 +5,7 @@ for /f "delims=" %%a in ('dir /b /ad') do (
     pushd "%%~a"
     set lastlang=
     set subtype=
+    set skip=
     for /f "delims=" %%b in ('dir ?_*.srt ??_*.srt /b /a-d') do (
         set "fn=%%~nb"
         set "num=!fn:~0,1!"
@@ -14,16 +15,21 @@ for /f "delims=" %%a in ('dir /b /ad') do (
             set "language=!fn:~3!"
         )
         if "!lastlang!" == "!language!" (
-            if "!subtype!" == "" (
-                set subtype=.SDH
-            ) else if "!subtype!" == ".SDH" (
-                set subtype=.Alt
-            ) else (
-                >&2 echo warning: more than 3 subtitles of !language!, skipped
-                set skip=1
+            if not defined skip (
+                if not defined subtype (
+                    set subtype=.SDH
+                ) else if "!subtype!" == ".SDH" (
+                    set subtype=.Alt
+                ) else (
+                    >&2 echo warning: more than 3 subtitles of !language!, skipped
+                    set skip=1
+                )
             )
+        ) else (
+            set subtype=
+            set skip=
         )
-        if "!skip!" NEQ "1" (
+        if not defined skip (
             set lastlang=!language!
             ren "%%~b" "%%~a.!language!!subtype!.srt"
             move "%%~a.!language!!subtype!.srt" "..\..\" 1>nul
